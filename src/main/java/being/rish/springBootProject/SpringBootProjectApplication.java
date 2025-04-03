c63edfc63fa2b19acfd34aefcb8c785476109f31
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-03 23:30:23 +0530

    @SpringBootApplication Internal working | IOC container and Dependency Injection in Spring boot

--- src/main/java/being/rish/springBootProject/SpringBootProjectApplication.java
+++ src/main/java/being/rish/springBootProject/SpringBootProjectApplication.java
@@ -3,7 +3,17 @@ package being.rish.springBootProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //works all the three annotations
+// @Configuration -> used with @Bean annotation is used for configuration
+// @EnableAutoConfiguration -> Automatic configure
+// @ComponentScan -> scan the IOC(Inversion of control)
+
+// Application context is a way to achieve IOC container
+// Bean -> Class is automatically registered as a Spring bean,
+// when we do @Component to any that particular class
+
+// We can create bean using @Configuration annotation to the class.
+// @Been annotation used to the methods only.
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
