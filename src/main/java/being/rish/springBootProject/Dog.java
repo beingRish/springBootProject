d09f1758796893b0d773a16e8b3d07d19cd66046
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-03 23:37:52 +0530

    @SpringBootApplication Internal working | IOC container and Dependency Injection in Spring boot

---
+++ src/main/java/being/rish/springBootProject/Dog.java
@@ -0,0 +1,10 @@
+package being.rish.springBootProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    public String fun() {
+        return "something";
+    }
+}
